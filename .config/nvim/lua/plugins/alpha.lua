a6964f433cf107bbea7de0ff3f2c2ddf03cc7b11
p4radi53 <adam.raj27@gmail.com>
2025-04-02 19:46:19 +0200

    new landing page

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -4,10 +4,62 @@ return {
   config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "New File", "<cmd>ene<CR>"),
-      dashboard.button("ff", "Find Files", "<cmd>Telescope find_files<CR>"),
+
+    -- Custom header with monkey ASCII art
+    dashboard.section.header.val = {
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣤⣤⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢾⣿⣿⣿⣿⣄⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣴⣿⣿⣶⣄⠹⣿⣿⣿⡟⠁⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⡆⢹⣿⣿⣿⣷⡀⠀",
+      "⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⣀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⠀⢿⣿⣿⣿⡇⠀",
+      "⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⢸⣿⣿⠟⠁⠀",
+      "⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠹⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⣿⣿⣿⣿⣿⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠀⢿⣿⣿⣿⣿⡄⠀⠀⠀⠀",
+      "⠀⠀⢀⣿⣿⣿⣿⣿⡟⢀⣿⣿⣿⣿⣿⣿⡿⠟⢁⡄⠸⣿⣿⣿⣿⣷⠀⠀⠀⠀",
+      "⠀⠀⣼⣿⣿⣿⣿⠏⠀⣈⡙⠛⢛⠋⠉⠁⠀⣸⣿⣿⠀⢻⣿⣿⣿⣿⡆⠀⠀⠀",
+      "⠀⢠⣿⣿⣿⣿⣟⠀⠀⢿⣿⣿⣿⡄⠀⠀⢀⣿⣿⡟⠃⣸⣿⣿⣿⣿⡇⠀⠀⠀",
+      "⠀⠘⠛⠛⠛⠛⠛⠛⠀⠘⠛⠛⠛⠛⠓⠀⠛⠛⠛⠃⠘⠛⠛⠛⠛⠛⠃⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
     }
+    -- Add the quote under the header
+    dashboard.section.footer.val = "embr4ce m0nke"
+    dashboard.section.footer.opts.hl = "Type"
+    -- Buttons
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("ff", "  Find file", ":Telescope find_files<CR>"),
+      dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
+      dashboard.button("fs", "  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+      dashboard.button("l", "  Lazy", ":Lazy<CR>"),
+      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    }
+
+    --Version
+    local v = vim.version()
+    local version_str = string.format("Neovim v%d.%d.%d", v.major, v.minor, v.patch)
+    dashboard.section.version = {
+      type = "text",
+      val = version_str,
+      opts = { hl = "Comment", position = "center" },
+    }
+
+    -- Adjust layout spacing
+    dashboard.config.layout = {
+      { type = "padding", val = 2 },
+      dashboard.section.header,
+      { type = "padding", val = 2 },
+      dashboard.section.buttons,
+      { type = "padding", val = 1 },
+      dashboard.section.footer,
+      { type = "padding", val = 1 },
+      dashboard.section.version,
+    }
+
+    -- Configure options
+    dashboard.opts.opts.noautocmd = true
 
     alpha.setup(dashboard.opts)
     vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
