80c63e08202ab74199f99309358cbeac02a459ce
p4radi53 <adam.raj27@gmail.com>
2025-04-08 20:38:49 +0200

    Disable pgup/pgdown

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -7,3 +7,7 @@ keymap.set("n", "<leader>wq", ":wq<CR>", { desc = "write and quit" })
 keymap.set("n", "<leader>w", ":write<CR>", { desc = "write" })
 keymap.set("n", "U", "<C-r>", { desc = "redo" })
 keymap.set("i", "jj", "<ESC>", { desc = "exit insert mode" })
+
+-- disable pgup/pgdown keys
+vim.keymap.set({ "n", "i" }, "<PageDown>", "<Nop>", { silent = true })
+vim.keymap.set({ "n", "i" }, "<PageUp>", "<Nop>", { silent = true })
